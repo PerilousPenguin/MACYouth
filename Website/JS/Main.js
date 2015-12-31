2e65e672be316534bcaaf1c3bbcf5d62f40bbf83
PerilousPenguin <osma.h.alprince@gmail.com>
2015-12-31 16:04:33 -0500

    initial commit

---
+++ Website/JS/Main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$("#menu").mouseenter(function() {
+		$("#menu").fadeTo(1000,1);
+	});
+	$("#menu").mouseleave(function() {
+		$("#menu").fadeTo(1000,0.8);
+	});
+	$("#").Something(function(){
+		
+	});
+});
