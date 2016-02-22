c736ef32c8214979d9268d150236f30aa6ee8851
PerilousPenguin <osma.h.alprince@gmail.com>
2016-02-21 20:54:25 -0500

    Thrown out the old Idea, this is the newer more professional look... Still needs some work.
    Thrown out the old Idea, this is the newer more professional look...
    Still needs some work.

--- Website/JS/Main.js
+++ Website/JS/Main.js
@@ -1,8 +1,32 @@
 $(document).ready(function() {
-	$(".menu").mouseenter(function() {
-		$(".menu").fadeTo(1000,1);
+		$(".about").mouseenter(function() {
+		$(".about").fadeTo(100,0.8);
 	});
-	$(".menu").mouseleave(function() {
-		$(".menu").fadeTo(1000,0.8);
+	$(".about").mouseleave(function() {
+		$(".about").fadeTo(100,1);
+	});
+		$(".sign").mouseenter(function() {
+		$(".sign").fadeTo(100,0.8);
+	});
+	$(".sign").mouseleave(function() {
+		$(".sign").fadeTo(100,1);
+	});
+		$(".volunteer").mouseenter(function() {
+		$(".volunteer").fadeTo(100,0.8);
+	});
+	$(".volunteer").mouseleave(function() {
+		$(".volunteer").fadeTo(100,1);
+	});
+		$(".contact").mouseenter(function() {
+		$(".contact").fadeTo(100,0.8);
+	});
+	$(".contact").mouseleave(function() {
+		$(".contact").fadeTo(100,1);
+	});
+	$(".events").mouseenter(function() {
+		$(".events").fadeTo(100,0.8);
+	});
+	$(".events").mouseleave(function() {
+		$(".events").fadeTo(100,1);
 	});
 });
